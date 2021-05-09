8638dccc697b51641482c7878d25ca0fa7508f0b
joseph-brewer-dev <JCBGames65@outlook.com>
2021-05-09 17:45:30 +0100

    v0.06 - Added support for concurrent use of the batch processor with the main program. v0.07 - Added support for use in parallel of the batch processor with the main program.

--- ConstGameEngine/Engine/data_management/BatchProcessing.py
+++ ConstGameEngine/Engine/data_management/BatchProcessing.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process
+from threading import Thread
 from typing import Callable
 
 
@@ -5,4 +7,15 @@ class BatchProcessing:
     @staticmethod
     def obtain_process(func: Callable, args: tuple) -> Callable:
         def f(): func(*args)
+
         return f
+
+    @staticmethod
+    def run_process_concurrent(func: Callable):
+        thread = Thread(target=func)
+        thread.start()
+
+    @staticmethod
+    def run_process_ASYNC(func: Callable):
+        process = Process(target=func)
+        process.start()
