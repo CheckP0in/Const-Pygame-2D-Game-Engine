e15af00bb8c7ece891e8984f02033de30d774cd3
joseph-brewer-dev <JCBGames65@outlook.com>
2021-05-29 16:22:51 +0100

    0.442 - Optimized EntityManager class to be able to use jit and njit compilation.
    HUGE NEWS:
    I am making officially implementing entity component system architecture to the Const
    Pygame 2D Game Engine. This implementation will be prioritized till it is fully
    implemented, so don't be surprised if the next few weeks will mostly be updates to
    the architecture.

--- ConstGameEngine/Engine/ecs/Entities/EntityManager.py
+++ ConstGameEngine/Engine/ecs/Entities/EntityManager.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit, njit
 
 
 class EntityManager:
@@ -12,21 +13,79 @@ class EntityManager:
     def get_entities(self):
         return self.entities
 
+    @jit(nopython=False)
+    def get_entities_jit(self):
+        return self.entities
+
+    @njit
+    def get_entities_njit(self):
+        return self.entities
+
     def set_entities(self, entities):
+        self.entities = entities
+
+    @jit(nopython=False)
+    def set_entities_jit(self, entities):
+        self.entities = entities
+
+    @njit
+    def set_entities_njit(self, entities):
         self.entities = entities
 
     def is_entity_in_list(self, ent):
         return ent in self.entities
 
+    @jit(nopython=False)
+    def is_entity_in_list_jit(self, ent):
+        return ent in self.entities
+
+    @njit
+    def is_entity_in_list_njit(self, ent):
+        return ent in self.entities
+
     def remove_entity(self, entity):
+        np.remove(self.entities, entity)
+
+    @jit(nopython=False)
+    def remove_entity_jit(self, entity):
+        np.remove(self.entities, entity)
+
+    @njit
+    def remove_entity_njit(self, entity):
         np.remove(self.entities, entity)
 
     def silent_remove_entity(self, entity):
         if entity in self.entities:
             np.remove(self.entities, entity)
 
+    @jit(nopython=False)
+    def silent_remove_entity_jit(self, entity):
+        if entity in self.entities:
+            np.remove(self.entities, entity)
+
+    @njit
+    def silent_remove_entity_njit(self, entity):
+        if entity in self.entities:
+            np.remove(self.entities, entity)
+
     def add_entity(self, entity):
         np.append(self.entities, entity)
 
+    @jit(nopython=False)
+    def add_entity_jit(self, entity):
+        np.append(self.entities, entity)
+
+    @njit
+    def add_entity_njit(self, entity):
+        np.append(self.entities, entity)
+
     def insert_entity(self, ind, entity):
+        np.insert(self.entities, ind, entity)
+
+    @jit(nopython=False)
+    def insert_entity_jit(self, ind, entity):
+        np.insert(self.entities, ind, entity)
+
+    @njit
+    def insert_entity_njit(self, ind, entity):
         np.insert(self.entities, ind, entity)
