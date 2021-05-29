a8ea8425f99fe65f4a2798b0402c6d5c7f24b6c8
joseph-brewer-dev <JCBGames65@outlook.com>
2021-05-29 14:32:44 +0100

    v0.44 - Optimized BatchProcessing class to be able to use jit and njit compilation.
    HUGE NEWS:
    I am making officially implementing entity component system architecture to the Const
    Pygame 2D Game Engine. This implementation will be prioritized till it is fully
    implemented, so don't be surprised if the next few weeks will mostly be updates to
    the architecture.

--- ConstGameEngine/Engine/data_management/BatchProcessing.py
+++ ConstGameEngine/Engine/data_management/BatchProcessing.py
@@ -2,6 +2,7 @@
 from multiprocessing import Process
 from threading import Thread
 from typing import Callable
+from numba import jit, njit
 
 
 # -- Create class -- #
@@ -13,14 +14,46 @@ class BatchProcessing:
 
         return f
 
+    @jit(nopython=False)
+    def obtain_process_jit(self, func: Callable, args: tuple) -> Callable:
+        def f(): func(*args)
+
+        return f
+
+    @njit
+    def obtain_process_njit(self, func: Callable, args: tuple) -> Callable:
+        def f(): func(*args)
+
+        return f
+
     # -- Used for running a process concurrently -- #
     @staticmethod
     def run_process_concurrent(func: Callable):
         thread = Thread(target=func)
         thread.start()
 
+    @jit(nopython=False)
+    def run_process_concurrent_jit(self, func: Callable):
+        thread = Thread(target=func)
+        thread.start()
+
+    @njit
+    def run_process_concurrent_njit(self, func: Callable):
+        thread = Thread(target=func)
+        thread.start()
+
     # -- Used for running a process in parallel with the rest of the program -- #
     @staticmethod
     def run_process_ASYNC(func: Callable):
+        process = Process(target=func)
+        process.start()
+
+    @jit(nopython=False)
+    def run_process_ASYNC_jit(self, func: Callable):
+        process = Process(target=func)
+        process.start()
+
+    @njit
+    def run_process_ASYNC_njit(self, func: Callable):
         process = Process(target=func)
         process.start()
