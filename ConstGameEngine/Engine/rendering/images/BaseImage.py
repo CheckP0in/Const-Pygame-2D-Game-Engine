3f2e79ede6401ea663f68ccfcd5248429a90907c
joseph-brewer-dev <JCBGames65@outlook.com>
2021-05-30 15:59:10 +0100

    0.445 - Optimized BaseImage class to be able to use jit and njit compilation. 0.446 - Optimized Image class to be able to use jit and njit compilation.
    HUGE NEWS:
    I am making officially implementing entity component system architecture to the Const
    Pygame 2D Game Engine. This implementation will be prioritized till it is fully
    implemented, so don't be surprised if the next few weeks will mostly be updates to
    the architecture.

--- ConstGameEngine/Engine/rendering/images/BaseImage.py
+++ ConstGameEngine/Engine/rendering/images/BaseImage.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+from numba import jit, njit
 
 
 class BaseImage:
@@ -17,8 +18,26 @@ class BaseImage:
         parsed_path: list = self.path.split('/')
         self.image = pygame.image.load(os.path.join(parsed_path[0], '/'.join(parsed_path[1:])))
 
+    @jit(nopython=False)
+    def init_jit(self):
+        parsed_path: list = self.path.split('/')
+        self.image = pygame.image.load(os.path.join(parsed_path[0], '/'.join(parsed_path[1:])))
+
+    @njit
+    def init_njit(self):
+        parsed_path: list = self.path.split('/')
+        self.image = pygame.image.load(os.path.join(parsed_path[0], '/'.join(parsed_path[1:])))
+
     # -- Draw the image -- #
     def draw_image(self, window: pygame.Surface):
+        window.blit(self.image, (self.x, self.y, self.width, self.height))
+
+    @jit(nopython=False)
+    def draw_image_jit(self, window: pygame.Surface):
+        window.blit(self.image, (self.x, self.y, self.width, self.height))
+
+    @njit
+    def draw_image_njit(self, window: pygame.Surface):
         window.blit(self.image, (self.x, self.y, self.width, self.height))
 
     # -- Rotate an image -- #
@@ -26,7 +45,23 @@ class BaseImage:
     def rotate_image(surface, angle):
         return pygame.transform.rotate(surface, angle)
 
+    @jit(nopython=False)
+    def rotate_image_jit(self, surface, angle):
+        return pygame.transform.rotate(surface, angle)
+
+    @njit
+    def rotate_image_njit(self, surface, angle):
+        return pygame.transform.rotate(surface, angle)
+
     # -- Scale an image -- #
     @staticmethod
     def scale_image(surface, width, height):
+        return pygame.transform.scale(surface, (width, height))
+
+    @jit(nopython=False)
+    def scale_image_jit(self, surface, width, height):
+        return pygame.transform.scale(surface, (width, height))
+
+    @njit
+    def scale_image_njit(self, surface, width, height):
         return pygame.transform.scale(surface, (width, height))
--- ConstGameEngine/Engine/rendering/images/Image.py
+++ ConstGameEngine/Engine/rendering/images/Image.py
@@ -1,6 +1,7 @@
 # -- Import Dependencies -- #
 import cv2
 import os
+from numba import jit, njit
 
 
 # -- Create class -- #
@@ -8,6 +9,14 @@ class Image:
     # -- Loads an image and returns a cv2 image object -- #
     @staticmethod
     def load(image_dir: str, mode):
+        return cv2.imread(image_dir, mode)
+
+    @jit(nopython=False)
+    def load_jit(self, image_dir: str, mode):
+        return cv2.imread(image_dir, mode)
+
+    @njit
+    def load_njit(self, image_dir: str, mode):
         return cv2.imread(image_dir, mode)
 
     # -- Resizes the image given to a new size or scale -- #
@@ -19,9 +28,33 @@ class Image:
         else:
             return cv2.resize(img, (width, height))
 
+    @jit(nopython=False)
+    def resize_jit(self, img: cv2.imread, width: int, height: int, width_multiplier=1.0, height_multiplier=1.0):
+        if (width_multiplier < 1.0 or width_multiplier > 1.0) and (height_multiplier < 1.0 or height_multiplier > 1.0):
+            return cv2.resize(img, (0, 0), fx=width_multiplier, fy=height_multiplier)
+
+        else:
+            return cv2.resize(img, (width, height))
+
+    @njit
+    def resize_njit(self, img: cv2.imread, width: int, height: int, width_multiplier=1.0, height_multiplier=1.0):
+        if (width_multiplier < 1.0 or width_multiplier > 1.0) and (height_multiplier < 1.0 or height_multiplier > 1.0):
+            return cv2.resize(img, (0, 0), fx=width_multiplier, fy=height_multiplier)
+
+        else:
+            return cv2.resize(img, (width, height))
+
     # -- rotates the given image to the new rotation -- #
     @staticmethod
     def rotate(img, rotation):
+        return cv2.rotate(img, rotation)
+
+    @jit(nopython=False)
+    def rotate_jit(self, img, rotation):
+        return cv2.rotate(img, rotation)
+
+    @njit
+    def rotate_njit(self, img, rotation):
         return cv2.rotate(img, rotation)
 
     # -- Saves the given image to the given directory. If the image already exists in the given directory, the image is replaced -- #
@@ -35,14 +68,50 @@ class Image:
         else:
             cv2.imwrite(final_dir, img)
 
+    @jit(nopython=False)
+    def save_jit(self, image_dir, img):
+        temp_dir = image_dir.split('/')
+        if os.path.exists(final_dir := os.path.join(temp_dir[0], '/'.join(temp_dir[1:]))):
+            os.remove(final_dir)
+            cv2.imwrite(final_dir, img)
+
+        else:
+            cv2.imwrite(final_dir, img)
+
+    @njit
+    def save_njit(self, image_dir, img):
+        temp_dir = image_dir.split('/')
+        if os.path.exists(final_dir := os.path.join(temp_dir[0], '/'.join(temp_dir[1:]))):
+            os.remove(final_dir)
+            cv2.imwrite(final_dir, img)
+
+        else:
+            cv2.imwrite(final_dir, img)
+
     # -- Gets the shape of the image as a list. [columns, rows, channels] -- #
     @staticmethod
     def get_image_shape(image) -> tuple:
         return image.shape
 
+    @jit(nopython=False)
+    def get_image_shape_jit(self, image) -> tuple:
+        return image.shape
+
+    @njit
+    def get_image_shape_njit(self, image) -> tuple:
+        return image.shape
+
     # -- Changes a pixel to a new colour -- #
     @staticmethod
     def change_pixel(image, pixel_col: int, pixel_row: int, colour: tuple[int, int, int]):
+        image[pixel_col][pixel_row] = list(colour)
+
+    @jit(nopython=False)
+    def change_pixel_jit(self, image, pixel_col: int, pixel_row: int, colour: tuple[int, int, int]):
+        image[pixel_col][pixel_row] = list(colour)
+
+    @njit
+    def change_pixel_njit(self, image, pixel_col: int, pixel_row: int, colour: tuple[int, int, int]):
         image[pixel_col][pixel_row] = list(colour)
 
     # -- changes a group of pixels to a new colour -- #
@@ -68,8 +137,64 @@ class Image:
                 for row in range(image[col][row_slice[0]:row_slice[1]]):
                     image[col][row] = list(colour)
 
+    @jit(nopython=False)
+    def change_pixel_group_jit(self, image, colour: tuple[int, int, int], rowI=0, colI=0, row_slice=None,
+                               col_slice=None):
+        if row_slice is None and col_slice is None:
+            for i in range(rowI):
+                for j in range(colI):
+                    image[i][j] = list(colour)
+
+        elif row_slice is not None and col_slice is None:
+            for row in range(image[row_slice[0]:row_slice[1]]):
+                for j in range(image.shape[1]):
+                    image[row][j] = list(colour)
+
+        elif row_slice is None and col_slice is not None:
+            for i in range(image.shape[0]):
+                for col in range(image[i][col_slice[0]:col_slice[1]]):
+                    image[i][col] = list(colour)
+
+        else:
+            for col in range(image[col_slice[0]:col_slice[1]]):
+                for row in range(image[col][row_slice[0]:row_slice[1]]):
+                    image[col][row] = list(colour)
+
+    @njit
+    def change_pixel_group_njit(self, image, colour: tuple[int, int, int], rowI=0, colI=0, row_slice=None,
+                                col_slice=None):
+        if row_slice is None and col_slice is None:
+            for i in range(rowI):
+                for j in range(colI):
+                    image[i][j] = list(colour)
+
+        elif row_slice is not None and col_slice is None:
+            for row in range(image[row_slice[0]:row_slice[1]]):
+                for j in range(image.shape[1]):
+                    image[row][j] = list(colour)
+
+        elif row_slice is None and col_slice is not None:
+            for i in range(image.shape[0]):
+                for col in range(image[i][col_slice[0]:col_slice[1]]):
+                    image[i][col] = list(colour)
+
+        else:
+            for col in range(image[col_slice[0]:col_slice[1]]):
+                for row in range(image[col][row_slice[0]:row_slice[1]]):
+                    image[col][row] = list(colour)
+
     # -- copies a slice of the image and pastes it to a new location -- #
     @staticmethod
     def copy_paste_image_section(image, start_col, end_col, size):
+        tag = image[start_col:start_col + size]
+        image[end_col:end_col + size] = tag
+
+    @jit(nopython=False)
+    def copy_paste_image_section_jit(self, image, start_col, end_col, size):
+        tag = image[start_col:start_col + size]
+        image[end_col:end_col + size] = tag
+
+    @njit
+    def copy_paste_image_section_njit(self, image, start_col, end_col, size):
         tag = image[start_col:start_col + size]
         image[end_col:end_col + size] = tag
