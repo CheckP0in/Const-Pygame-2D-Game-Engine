5a2adf2b25e1f2293806b72c251f15bb4a79423b
joseph-brewer-dev <JCBGames65@outlook.com>
2021-05-30 16:19:13 +0100

    0.447 - Optimized PygameText class to be able to use jit and njit compilation.
    HUGE NEWS:
    I am making officially implementing entity component system architecture to the Const
    Pygame 2D Game Engine. This implementation will be prioritized till it is fully
    implemented, so don't be surprised if the next few weeks will mostly be updates to
    the architecture.

--- ConstGameEngine/Engine/rendering/text/PygameText.py
+++ ConstGameEngine/Engine/rendering/text/PygameText.py
@@ -1,5 +1,6 @@
 # -- Import dependencies -- #
 import pygame
+from numba import jit, njit
 
 
 # -- Create class
@@ -9,9 +10,25 @@ class PygameText:
     def init_sys_font(font_name='New Times Roman', font_size=28, bold=False, italic=False):
         return pygame.font.SysFont(font_name, font_size, bold=bold, italic=italic)
 
+    @jit(nopython=False)
+    def init_sys_font_jit(self, font_name='New Times Roman', font_size=28, bold=False, italic=False):
+        return pygame.font.SysFont(font_name, font_size, bold=bold, italic=italic)
+
+    @jit
+    def init_sys_font_njit(self, font_name='New Times Roman', font_size=28, bold=False, italic=False):
+        return pygame.font.SysFont(font_name, font_size, bold=bold, italic=italic)
+
     # -- Initialize a custom font -- #
     @staticmethod
     def init_custom_font(path=None, font_size=28):
+        return pygame.font.Font(path, font_size)
+
+    @jit(nopython=False)
+    def init_custom_font_jit(self, path=None, font_size=28):
+        return pygame.font.Font(path, font_size)
+
+    @njit
+    def init_custom_font_njit(self, path=None, font_size=28):
         return pygame.font.Font(path, font_size)
 
     # -- Gets all available system fonts -- #
@@ -19,12 +36,36 @@ class PygameText:
     def get_sys_fonts():
         return pygame.font.get_fonts()
 
+    @jit(nopython=False)
+    def get_sys_fonts_jit(self):
+        return pygame.font.get_fonts()
+
+    @njit
+    def get_sys_fonts_njit(self):
+        return pygame.font.get_fonts()
+
     # -- Render a font -- #
     @staticmethod
     def render_font(text, font, colour, antialias=True):
         return font.render(text, antialias=antialias, colour=colour)
 
+    @jit(nopython=False)
+    def render_font_jit(self, text, font, colour, antialias=True):
+        return font.render(text, antialias=antialias, colour=colour)
+
+    @njit
+    def render_font_njit(self, text, font, colour, antialias=True):
+        return font.render(text, antialias=antialias, colour=colour)
+
     # -- Renders text to the screen -- #
     @staticmethod
     def draw_text(surface, rendered_font, x, y):
+        surface.blit(rendered_font, (x, y))
+
+    @jit(nopython=False)
+    def draw_text_jit(self, surface, rendered_font, x, y):
+        surface.blit(rendered_font, (x, y))
+
+    @njit
+    def draw_text_njit(self, surface, rendered_font, x, y):
         surface.blit(rendered_font, (x, y))
